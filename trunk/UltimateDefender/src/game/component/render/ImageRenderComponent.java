57aecdd7c43ab94b33b63f66118b6c24aff7fb46
aoki.ec.usp@gmail.com <aoki.ec.usp@gmail.com@2a1bd22d-7202-5b1b-770d-88821e24a0cc>
2011-10-25 02:40:44 +0000

    24/10~25/10

--- trunk/UltimateDefender/src/game/component/render/ImageRenderComponent.java
+++ trunk/UltimateDefender/src/game/component/render/ImageRenderComponent.java
@@ -2,13 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package game.gui;
+package game.component.render;
 
 /**
  *
  * @author Lucas
  */
-public interface TimerListener {
-
-    public void update(Timer timer);
+public class ImageRenderComponent {
+    
 }
