b85ceed0775c28cc4203565713f49375d6f50608
aoki.ec.usp@gmail.com <aoki.ec.usp@gmail.com@2a1bd22d-7202-5b1b-770d-88821e24a0cc>
2011-10-27 15:33:37 +0000

    26/10

---
+++ trunk/UltimateDefender/src/game/component/render/AnimationRenderComponent.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.component.render;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Lucas
+ */
+public class AnimationRenderComponent extends RenderComponent{
+
+    public AnimationRenderComponent(String id) {
+        super(id);
+    }
+    
+    @Override
+    public void render(Graphics gr) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
