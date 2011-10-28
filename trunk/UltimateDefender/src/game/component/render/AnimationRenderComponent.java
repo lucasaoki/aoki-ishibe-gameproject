b83f88dfc2176c863c1d378f3d42360273d75e10
aoki.ec.usp@gmail.com <aoki.ec.usp@gmail.com@2a1bd22d-7202-5b1b-770d-88821e24a0cc>
2011-10-28 12:04:40 +0000

    28/10

--- trunk/UltimateDefender/src/game/component/render/AnimationRenderComponent.java
+++ trunk/UltimateDefender/src/game/component/render/AnimationRenderComponent.java
@@ -4,26 +4,48 @@
  */
 package game.component.render;
 
+import game.component.Component;
+import game.component.characters.CharacterInfo;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
 
 /**
  *
  * @author Lucas
  */
 public class AnimationRenderComponent extends RenderComponent{
+    private CharacterInfo characterInfo = null;
+    private Image[] animation;
+    private Image[] currentAnimation;
 
-    public AnimationRenderComponent(String id) {
+    public AnimationRenderComponent(String id, Component characterInfo, Image[] imageFrames) {
         super(id);
+        if(characterInfo instanceof CharacterInfo)
+            this.characterInfo = (CharacterInfo)characterInfo;
+        
+    }
+    
+    public void setAnimation(Image[] animation){
+        this.currentAnimation = animation;
     }
     
     @Override
     public void render(Graphics gr) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Point pos = owner.getPosition();
+        /*gr.drawImage(null, pos.x, pos.y, null);*/
     }
 
     @Override
     public void update() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (characterInfo.isJumping()) {
+            //acertar frame inicial, para não rodar sequencia errada da animação.
+//            currentAnimation = this.animation[2];
+        } else if (characterInfo.isWalkingR()) {
+//            currentAnimation = this.animation[0];
+        } else {
+//            currentAnimation = this.animation[1];
+        }
     }
     
 }
