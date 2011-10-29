c8a04580e77bac9271f08d9a5e683bdd5d559137
aoki.ec.usp@gmail.com <aoki.ec.usp@gmail.com@2a1bd22d-7202-5b1b-770d-88821e24a0cc>
2011-10-29 00:54:38 +0000

    28/10

--- UltimateDefender/src/game/component/characters/Kenshin.java
+++ UltimateDefender/src/game/component/characters/Kenshin.java
@@ -4,9 +4,8 @@
  */
 package game.component.characters;
 
-import game.component.Component;
-import game.component.movement.playerMoviment;
 import game.entity.Entity;
+import java.awt.Image;
 
 /**
  *
@@ -14,8 +13,22 @@ import game.entity.Entity;
  */
 public class Kenshin extends Entity{
 
+    Image images[][];
+    
     public Kenshin(String id) {
         super(id);
-        this.addComponent(new playerMoviment());
+        this.addComponent(new CharacterInfo(id));
+        
+        images = new Image[10][];
+        images[0] = new Image[5];
     }
+    
+//    public Image[] read(String path, int num){
+//        Image tmp[] = new Image[num];
+//        for (int i = 0; i < imagens.length; i++) {
+//            String filename = "/images/pac" + i + ".png";
+//            URL url = GuiPacman.class.getResource(filename);
+//            imagens[i] = (new ImageIcon(url)).getImage();
+//        }        
+//    }
 }
